2476607b3ffb096ef47dc326afefc362366d213d
latifur <latifur@bs-23.net>
2023-04-03 04:09:20 +0600

    TITLE AND META TAG ADDED ON HEAD

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <title>Md Latifur Rahman</title>
+          <meta charset="UTF-8"/>
+          <meta name="description" content="This Is a Profile Site Of Md Latifur Rahman"/>
+          <meta name="keywords" content="Md Latifur rahman, UI engineer, Nopstation , HTML, CSS, JavaScript"/>
+          <meta name="author" content="MD LATIFUR RAHMAN"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet" />  
       </Head>
       <body>
